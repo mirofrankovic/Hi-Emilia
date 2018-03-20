eba85bea33f426731883d0d3057c6ece43547e9c
Miroslav frankovic <miro.frankovic@gmail.com>
2018-03-20 22:27:24 +0000

    Commited new files to my repository.

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("p").click(function(){
+     $(this).hide();
+    });
+    });
+
+
+
+
+/*var animal = "T-rex";
+document.getElementById("rex").innerHTML = animal;*/
