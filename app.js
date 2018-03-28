2450e81cb7ba21079e2e7acbbc0603a6c1c0ff50
Miroslav frankovic <miro.frankovic@gmail.com>
2018-03-28 23:15:07 +0100

    Commited new exercise to my repository.

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ $(document).ready(function(){
      $(this).hide();
     });
 });
-
+function run () {
+    alert ("You Clicked!");
 var rex = document.getElementById("rex");
+rex.innerHTML = "You Clicked!";
+};
 console.log(rex.style.color);
 rex.style.color = "magenta"
 
